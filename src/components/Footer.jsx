0673fded42952bb0460689a33dbb760a98619360
anumnoor643-jpg <anumnoor643@gmail.com>
2026-06-06 17:06:59 +0500

    Add Footer component with copyright information

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+<footer className="border-t border-gray-200 mt-12 py-6 text-center text-gray-500">
+  <p className="text-lg">
+    © 2026 Tiger Group of Companies. All Rights Reserved. |
+    Designed by{" "}
+    <a
+      href="https://www.bytexsolution.com"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="text-amber-700 font-bold hover:underline"
+    >
+      ByteX Solution
+    </a>{" "}
+    (Anum Noor)
+  </p>
+</footer>
